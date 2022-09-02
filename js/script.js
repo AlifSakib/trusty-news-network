e4333d87b3757894730d993f7670c71d2b9455ac
AlifSakib <alifsakib@gmail.com>
2022-09-02 22:23:15 +0600

    added category list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("Added");
+//Loading Catagories
+
+const loadCatagories = async () => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/news/categories`
+    );
+    const data = await res.json();
+    displayCatagories(data.data.news_category);
+  } catch (erroe) {
+    console.log(erroe);
+  }
+};
+
+const displayCatagories = (categories) => {
+  console.log(categories[0]);
+  const catagoriesContainer = document.getElementById("catagories-container");
+
+  categories.forEach((category) => {
+    const { category_id: id, category_name: name } = category;
+    const catagorieList = document.createElement("li");
+    catagorieList.innerHTML = `
+    <button>${name}</button>
+    `;
+    catagoriesContainer.appendChild(catagorieList);
+  });
+};
+
+loadCatagories();
