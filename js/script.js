a7054def7b254f534708f905b185413fdbabd9d8
AlifSakib <alifsakib@gmail.com>
2022-09-03 05:07:14 +0600

    mobile responsive done

--- js/script.js
+++ js/script.js
@@ -35,8 +35,6 @@ const getCategoryId = (category_id) => {
   spinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
 
-  console.log(url);
-
   const categoriesNames = document.getElementsByClassName("category-btn");
   for (const categoriesName of categoriesNames) {
     categoriesName.addEventListener("click", function () {
@@ -49,7 +47,7 @@ const getCategoryId = (category_id) => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+
       displayNews(data.data);
     } catch (erroe) {
       console.log(erroe);
@@ -83,7 +81,7 @@ const getCategoryId = (category_id) => {
       const newsDiv = document.createElement("div");
 
       newsDiv.innerHTML = `
-      <div class="card card-side bg-base-100 shadow-xl my-10">
+      <div class="card card-side bg-base-100 shadow-xl my-10 flex flex-col md:flex-row">
             <figure class="px-6 py-6">
               <img
                 class="rounded-lg"
@@ -96,7 +94,7 @@ const getCategoryId = (category_id) => {
               <p>${details.slice(0, 200)}</p>
               <p>${details.slice(200, 300)}</p>
               <!-- Article Detils -->
-              <div class="flex justify-between items-center">
+              <div class="flex justify-between items-center flex-col md:flex-row space-y-6 md:space-y-0 mt-6 md:mt-0">
                 <!-- Author  -->
                 <div class="flex items-center gap-4">
                   <img class="h-10 rounded-full" src="${img}" alt="" />
@@ -126,7 +124,11 @@ const getCategoryId = (category_id) => {
 
                 <!-- Button -->
                 <div class="card-actions">
-                  <button onclick="showMore('${_id}')" class="btn btn-primary">Show More</button>
+                  <button onclick="showMore('${_id}')" class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button"
+                data-modal-toggle="extralarge-modal"
+                >
+                Extra large modal</button>
                 </div>
                 <!-- Author  End-->
               </div>
@@ -158,14 +160,110 @@ const showMore = (news_id) => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      displayDetails(data);
+      displayDetails(data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
   const displayDetails = (details) => {
-    console.log(details);
+    details.forEach((detail) => {
+      console.log(detail);
+
+      const { title, image_url: thumb, details } = detail;
+      const modalContainer = document.getElementById("modal");
+
+      modalContainer.innerHTML = `
+      
+      <div
+          id="extralarge-modal"
+          tabindex="-1"
+          class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full flex justify-center items-center"
+        >
+          <div class="relative p-4 w-full max-w-7xl h-full md:h-auto">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <!-- Modal header -->
+              <div
+                class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600"
+              >
+                <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                  ${title}
+                </h3>
+                <button
+                  type="button"
+                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-toggle="extralarge-modal"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+              </div>
+              <!-- Modal body -->
+
+              <div class="p-6 space-y-6">
+                <div class="">
+                <img class="w-full h-96 object-cover" src="${thumb}">
+                </div>
+                <p
+                  class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                >
+                  ${details.slice(0, 200)}
+                </p>
+                <p
+                  class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                >
+                ${details.slice(200, 400)}
+                </p>
+                <p
+                  class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                >
+                ${details.slice(400)}
+                </p>
+              </div>
+              <!-- Modal footer -->
+              <div
+                class="flex justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600"
+              >
+                <button
+                  onclick="closeModal()"
+                  data-modal-toggle="extralarge-modal"
+                  type="button"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Close
+                </button>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      `;
+    });
+    showModal();
   };
   newsDetails();
 };
+
+function showModal() {
+  const showModal = document.getElementById("extralarge-modal");
+  showModal.classList.remove("hidden");
+}
+
+function closeModal() {
+  const closeModal = document.getElementById("extralarge-modal");
+  closeModal.classList.add("hidden");
+}
