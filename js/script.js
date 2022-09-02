24e10e441d998b56e93b5441af298b1f5eb21a0e
AlifSakib <alifsakib@gmail.com>
2022-09-03 03:09:37 +0600

    spinner added

--- js/script.js
+++ js/script.js
@@ -35,6 +35,8 @@ const getCategoryId = (category_id) => {
   spinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
 
+  console.log(url);
+
   const categoriesNames = document.getElementsByClassName("category-btn");
   for (const categoriesName of categoriesNames) {
     categoriesName.addEventListener("click", function () {
@@ -53,6 +55,7 @@ const getCategoryId = (category_id) => {
       console.log(erroe);
     }
   };
+
   const displayNews = (news) => {
     const articleContainer = document.getElementById("articles-container");
     const countNews = document.getElementById("numbers-of-news");
@@ -62,6 +65,7 @@ const getCategoryId = (category_id) => {
 
     articleContainer.innerHTML = "";
     let count = 0;
+
     news.forEach((data) => {
       count += 1;
       countNews.innerText = count;
@@ -72,7 +76,9 @@ const getCategoryId = (category_id) => {
         thumbnail_url: image,
 
         total_view: views,
+        _id,
         author: { name, img, published_date },
+        rating: { number, badge },
       } = data;
       const newsDiv = document.createElement("div");
 
@@ -96,7 +102,10 @@ const getCategoryId = (category_id) => {
                   <img class="h-10 rounded-full" src="${img}" alt="" />
                   <div>
                   <p>${name ? name : "Author Not Found"}</p>
-                  <p>${published_date ? published_date : "No Date Found"}</p>
+                  <p>${(published_date
+                    ? published_date
+                    : "No Date Found"
+                  ).slice(0, 10)}</p>
                   </div>
                 </div>
                 <!-- Article Views -->
@@ -107,16 +116,17 @@ const getCategoryId = (category_id) => {
                 </div>
 
                 <!-- Ratings -->
-                <div class="rating gap-1">
-                <input type="radio" name="rating-3" class="mask mask-heart bg-red-400" />
-                <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" checked />
-                <input type="radio" name="rating-3" class="mask mask-heart bg-yellow-400" />
-                <input type="radio" name="rating-3" class="mask mask-heart bg-lime-400" />
-                <input type="radio" name="rating-3" class="mask mask-heart bg-green-400" />
+                
+                <div class="flex items-center">
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">${number}</p>
+                    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                    <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">${badge}</a>
                 </div>
+
                 <!-- Button -->
                 <div class="card-actions">
-                  <button class="btn btn-primary">Details</button>
+                  <button onclick="showMore('${_id}')" class="btn btn-primary">Show More</button>
                 </div>
                 <!-- Author  End-->
               </div>
@@ -140,3 +150,22 @@ function spinner(isLoading) {
     spinner.classList.add("hidden");
   }
 }
+
+const showMore = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+
+  const newsDetails = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      displayDetails(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const displayDetails = (details) => {
+    console.log(details);
+  };
+  newsDetails();
+};
