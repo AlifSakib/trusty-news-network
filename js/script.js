b45d892774e338717293005c29f9560aab476ba8
AlifSakib <alifsakib@gmail.com>
2022-09-03 02:10:26 +0600

    dynamic count and category added

--- js/script.js
+++ js/script.js
@@ -13,14 +13,13 @@ const loadCatagories = async () => {
 };
 
 const displayCatagories = (categories) => {
-  console.log(categories[0]);
   const catagoriesContainer = document.getElementById("catagories-container");
 
   categories.forEach((category) => {
     const { category_id: id, category_name: name } = category;
     const catagorieList = document.createElement("li");
     catagorieList.innerHTML = `
-    <button onclick="getCategoryId('${id}')">${name}</button>
+    <button class="category-btn" onclick="getCategoryId('${id}')">${name}</button>
     `;
     catagoriesContainer.appendChild(catagorieList);
   });
@@ -29,16 +28,26 @@ const displayCatagories = (categories) => {
 loadCatagories();
 
 // function getCategoryId(id) {
-//   console.log(id);
+// console.log(id);
 // }
 
 const getCategoryId = (category_id) => {
+  spinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+
+  const categoriesNames = document.getElementsByClassName("category-btn");
+  for (const categoriesName of categoriesNames) {
+    categoriesName.addEventListener("click", function () {
+      const name = document.getElementById("category-name");
+      name.innerText = categoriesName.innerText;
+    });
+  }
 
   const loadNews = async () => {
     try {
       const res = await fetch(url);
       const data = await res.json();
+      console.log(data);
       displayNews(data.data);
     } catch (erroe) {
       console.log(erroe);
@@ -46,9 +55,16 @@ const getCategoryId = (category_id) => {
   };
   const displayNews = (news) => {
     const articleContainer = document.getElementById("articles-container");
+    const countNews = document.getElementById("numbers-of-news");
+    if (news.length === 0) {
+      countNews.innerText = 0;
+    }
+
     articleContainer.innerHTML = "";
+    let count = 0;
     news.forEach((data) => {
-      console.log(data);
+      count += 1;
+      countNews.innerText = count;
 
       const {
         title,
@@ -74,7 +90,7 @@ const getCategoryId = (category_id) => {
               <p>${details.slice(0, 200)}</p>
               <p>${details.slice(200, 300)}</p>
               <!-- Article Detils -->
-              <div class="flex justify-between">
+              <div class="flex justify-between items-center">
                 <!-- Author  -->
                 <div class="flex items-center gap-4">
                   <img class="h-10 rounded-full" src="${img}" alt="" />
@@ -89,6 +105,16 @@ const getCategoryId = (category_id) => {
                   <i class="fa-regular fa-eye"></i>
                   <p>${views ? views : "Not Found"}</p>
                 </div>
+
+                <!-- Ratings -->
+                <div class="rating gap-1">
+                <input type="radio" name="rating-3" class="mask mask-heart bg-red-400" />
+                <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" checked />
+                <input type="radio" name="rating-3" class="mask mask-heart bg-yellow-400" />
+                <input type="radio" name="rating-3" class="mask mask-heart bg-lime-400" />
+                <input type="radio" name="rating-3" class="mask mask-heart bg-green-400" />
+                </div>
+                <!-- Button -->
                 <div class="card-actions">
                   <button class="btn btn-primary">Details</button>
                 </div>
@@ -100,7 +126,17 @@ const getCategoryId = (category_id) => {
       `;
       articleContainer.appendChild(newsDiv);
     });
+    spinner(false);
   };
 
   loadNews();
 };
+
+function spinner(isLoading) {
+  const spinner = document.getElementById("spinner");
+  if (isLoading) {
+    spinner.classList.remove("hidden");
+  } else {
+    spinner.classList.add("hidden");
+  }
+}
