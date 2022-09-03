039a6bb097ecd969aee85ffdb1403d202dbf0e19
AlifSakib <alifsakib@gmail.com>
2022-09-03 19:04:42 +0600

    changed bg color for modal

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ const getCategoryId = (category_id) => {
       const newsDiv = document.createElement("div");
 
       newsDiv.innerHTML = `
-       <div class="card card-side bg-base-100 shadow-xl my-10 flex flex-col md:flex-row">
+       <div class="card card-side bg-base-100 shadow-xl my-10 flex flex-col md:flex-row ">
             <figure class="px-6 py-6">
                <img
                 class="rounded-lg md:h-64 md:w-48"
@@ -116,7 +116,7 @@ const getCategoryId = (category_id) => {
 
                  <!-- Ratings -->
 
-                 <div class="flex lg:flex items-center md:hidden ">
+                 <div class="flex lg:flex items-center md:hidden">
                      <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                      <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">${number}</p>
                      <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
@@ -176,10 +176,11 @@ const showMore = (news_id) => {
 
       modalContainer.innerHTML = `
 
+      
       <div
           id="extralarge-modal"
           tabindex="-1"
-          class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full flex justify-center items-center"
+          class=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full flex justify-center items-center "
         >
           <div class="relative p-4 w-full max-w-7xl h-full md:h-auto">
             <!-- Modal content -->
@@ -188,29 +189,10 @@ const showMore = (news_id) => {
               <div
                 class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600"
               >
-                <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                <h3 class="text-2xl font-bold text-gray-900 dark:text-white">
                   ${title}
                 </h3>
-                <button
-                  type="button"
-                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                  data-modal-toggle="extralarge-modal"
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span class="sr-only">Close modal</span>
-                </button>
+                
               </div>
               <!-- Modal body -->
 
@@ -231,7 +213,7 @@ const showMore = (news_id) => {
                 <p
                   class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
                 >
-                ${details.slice(400)}
+                ${details.slice(400, 1200)}
                 </p>
               </div>
               <!-- Modal footer -->
