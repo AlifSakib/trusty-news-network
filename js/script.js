1787b47abbec7972768e7bb9b46d592690630bb7
AlifSakib <alifsakib@gmail.com>
2022-09-03 08:00:25 +0600

    Sorted Data

--- js/script.js
+++ js/script.js
@@ -27,21 +27,9 @@ const displayCatagories = (categories) => {
 
 loadCatagories();
 
-// function getCategoryId(id) {
-// console.log(id);
-// }
-
 const getCategoryId = (category_id) => {
   spinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-
-  /* const categoriesNames = document.getElementsByClassName("category-btn");
-  for (const categoriesName of categoriesNames) {
-    categoriesName.addEventListener("click", function () {
-      const name = document.getElementById("category-name");
-      name.innerText = categoriesName.innerText;
-    });
-  } */
 
   const loadNews = async () => {
     try {
@@ -58,13 +46,14 @@ const getCategoryId = (category_id) => {
     const articleContainer = document.getElementById("articles-container");
     const countContainer = document.getElementById("count-container");
     const countNews = document.getElementById("numbers-of-news");
-
+    news.sort(function (a, b) {
+      return b.total_view - a.total_view;
+    });
+    articleContainer.innerHTML = "";
+    let count = 0;
     if (news.length === 0) {
       countNews.innerText = 0;
     }
-
-    articleContainer.innerHTML = "";
-    let count = 0;
 
     news.forEach((data) => {
       count += 1;
@@ -90,64 +79,62 @@ const getCategoryId = (category_id) => {
       const newsDiv = document.createElement("div");
 
       newsDiv.innerHTML = `
-      <div class="card card-side bg-base-100 shadow-xl my-10 flex flex-col md:flex-row">
+       <div class="card card-side bg-base-100 shadow-xl my-10 flex flex-col md:flex-row">
             <figure class="px-6 py-6">
-              <img
+               <img
                 class="rounded-lg"
-                src="${image}"
-                alt="newsThumb"
-              />
-            </figure>
+                 src="${image}"
+                 alt="newsThumb"
+               />
+             </figure>
             <div class="card-body">
-              <h2 class="card-title">${title}</h2>
-              <p>${details.slice(0, 200)}</p>
-              <p>${details.slice(200, 300)}</p>
-              <!-- Article Detils -->
-              <div class="flex justify-between items-center flex-col md:flex-row space-y-6 md:space-y-0 mt-6 md:mt-0">
-                <!-- Author  -->
-                <div class="flex items-center gap-4">
-                  <img class="h-10 rounded-full" src="${img}" alt="" />
-                  <div>
-                  <p>${name ? name : "Author Not Found"}</p>
-                  <p>${(published_date
-                    ? published_date
-                    : "No Date Found"
-                  ).slice(0, 10)}</p>
-                  </div>
-                </div>
-                <!-- Article Views -->
+               <h2 class="card-title">${title}</h2>
+               <p>${details.slice(0, 200)}</p>
+               <p>${details.slice(200, 300)}</p>
+               <!-- Article Detils -->
+               <div class="flex justify-between items-center flex-col md:flex-row space-y-6 md:space-y-0 mt-6 md:mt-0">
+                 <!-- Author  -->
+                 <div class="flex items-center gap-4">
+                   <img class="h-10 rounded-full" src="${img}" alt="" />
+                   <div>
+                   <p>${name ? name : "Author Not Found"}</p>
+                   <p>${(published_date
+                     ? published_date
+                     : "No Date Found"
+                   ).slice(0, 10)}</p>
+                   </div>
+                 </div>
+                 <!-- Article Views -->
 
-                <div class="flex items-center gap-2">
-                  <i class="fa-regular fa-eye"></i>
-                  <p>${views ? views : "Not Found"}</p>
-                </div>
+                 <div class="flex items-center gap-2">
+                   <i class="fa-regular fa-eye"></i>
+                   <p>${views ? views : "Not Found"}</p>
+                 </div>
 
-                <!-- Ratings -->
-                
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">${number}</p>
-                    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                    <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">${badge}</a>
-                </div>
+                 <!-- Ratings -->
 
-                <!-- Button -->
-                <div class="card-actions">
-                  <button onclick="showMore('${_id}')" class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-                data-modal-toggle="extralarge-modal"
-                >
-                Extra large modal</button>
-                </div>
-                <!-- Author  End-->
-              </div>
-              <!-- Article end -->
-            </div>
-          </div>
-      `;
+                 <div class="flex items-center">
+                     <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                     <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">${number}</p>
+                     <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                     <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">${badge}</a>
+                 </div>
+
+                 <!-- Button -->
+                 <div class="card-actions">
+                   <button onclick="showMore('${_id}')" class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                 type="button"
+                 data-modal-toggle="extralarge-modal"
+                 >
+                 Extra large modal</button>
+                 </div>
+                 <!-- Author  End-->
+               </div>
+               <!-- Article end -->
+             </div>
+           </div>`;
       articleContainer.appendChild(newsDiv);
     });
-
     spinner(false);
   };
 
@@ -185,7 +172,7 @@ const showMore = (news_id) => {
       const modalContainer = document.getElementById("modal");
 
       modalContainer.innerHTML = `
-      
+
       <div
           id="extralarge-modal"
           tabindex="-1"
@@ -256,12 +243,12 @@ const showMore = (news_id) => {
                 >
                   Close
                 </button>
-                
+
               </div>
             </div>
           </div>
         </div>
-      
+
       `;
     });
     showModal();
